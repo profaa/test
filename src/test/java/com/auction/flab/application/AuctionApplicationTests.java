7e808cb574f123c83fb89356aaa0595f3dc9b846
profaa <shyphic@naver.com>
2023-06-02 00:11:55 +0900

    first commit

---
+++ src/test/java/com/auction/flab/application/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auction.flab.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
